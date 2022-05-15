01f2b4f6d5e201f124f2fdaf6a055e224fb2d27a
Mia Dugas <msmiadugas@gmail.com>
2022-05-14 18:11:58 -0600

    added aliases with the jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,1 +1,23 @@
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
+//import Image from 'next/image';
 
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+        </h1>
+      </div>
+      {/* <div className={styles.errorText}></div> */}
+      <div className={styles.errorText}>
+        <h2>404</h2>
+        <h4>Like god, this page also does not exist</h4>
+        <Link href='/'>Go Back Home</Link>
+      </div>
+    </Layout>
+  );
+}
