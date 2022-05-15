3feed88b3c702448a10b83db207f0d16b866d4e8
Mia Dugas <msmiadugas@gmail.com>
2022-05-14 22:33:54 -0600

    testing api routes

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,6 +17,9 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='keywords' content={keywords} />
       </Head>
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Congregation Events</h1>
+      <h2>Find info on our events</h2>
+    </div>
+  );
+}
