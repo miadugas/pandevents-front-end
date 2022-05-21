4dd01cc8ba1807547caf75f6273ca0720ef93441
Mia Dugas <msmiadugas@gmail.com>
2022-05-21 16:19:49 -0600

    auth context logic

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -9,10 +9,29 @@ export const AuthProvider = ({ children }) => {
   const [error, setError] = useState(null)
 
   // Register user
+  const register = async (user) => {
+    console.log(user)
+  }
 
   // Login user
+  const login = async ({ email: identifier, password }) => {
+    console.log(identifier, password)
+  }
 
   // Logout user
-
+  const logout = async () => {
+    console.log('Logout')
+  }
   // Check if user is logged in
+  const checkUserLoggedIn = async (user) => {
+    console.log('Check')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
+
+export default AuthContext
